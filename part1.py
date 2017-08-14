d2a720e67886db8f12b8a9174b527fbc6646ef99
Candace Laptop <candacelax@gmail.com>
2017-08-14 15:00:34 -0400

    finished python code for pt1; double check implementation & print visuals

--- part1.py
+++ part1.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
-import numpy as np
+import numpy as np # this package is used for most mathematical operations
 
-# part 1
-
-# define dimensions
+# define input/output dimensions
 M = 10
 N = 50
 
 # define weight matrix
-W = np.rand()
+W = np.random.rand(M,N)
 W = 2*(W-0.5)
 
 # TODO check dimensions
-x = np.rand(50)
+x = np.random.rand(50) # random is the numpy package used to generate random numbers
 
 y = W*x
