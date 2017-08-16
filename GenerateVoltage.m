2d0926c78ab214e3662ea3f2a57356ac8815c8dc
Candace <ccross@mit.edu>
2017-08-16 12:00:59 -0400

    initial commit for this function for part 2 in MATLAB

---
+++ GenerateVoltage.m
@@ -0,0 +1,15 @@
+function V = GenerateVoltage(p,T,Vreset,Vthresh,V0)
+
+V = zeros(1,T);
+V(1) = V0;
+for t = 1:T-1
+    if V(t) == Vthresh
+        V(t+1) = Vreset;
+    else
+        dV = ((rand(1)<p)-0.5)*2;
+        V(t+1) = V(t) + dV;
+    end
+    if V(t+1) > Vthresh
+        V(t+1) = Vthresh;
+    end
+end
