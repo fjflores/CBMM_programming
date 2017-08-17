da48328e38fa89e03b996685753ffab9d7235636
Francisco J. Flores <fjflores@neurostat.mit.edu>
2017-08-17 15:34:02 -0400

    Added comments to part1 excercise.

--- part1.m
+++ part1.m
@@ -1,11 +1,19 @@
 %% Part 1: Matrix Operations for a Feedforward Network
 clear all
+
+% M is the number of rows, and N is the number of columns.
 M = 10;
 N = 50;
 
-W = rand(M,N);
-W = 2.*(W - 0.5);
+% Generate a random number between 0 and 1, drawed from a uniform 
+% distribution.
+W = rand( M, N );
 
-x = randn(1,50)';
+% Map the numbers between [0,1] to numbers between [-1,1].
+W = 2 .* ( W - 0.5 );
 
-y = W*x;
+% Generate a column input vector x with 50 elements.
+x = randn( 50, 1 );
+
+% Compute the output of the network.
+y = W * x;
