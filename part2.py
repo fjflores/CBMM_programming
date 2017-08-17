4f067b4e2f61fd4c06829edf278db0778edb8fc8
Candace Ross <candacelax@gmail.com>
2017-08-17 14:39:33 -0400

    working

--- part2.py
+++ part2.py
@@ -11,13 +11,15 @@ def generateVoltage(p, T, V_reset, V_thresh, V_0):
         if V[t] == V_thresh:
             V[t+1] = V_reset
         else:
-            dV = ( (np.random.rand(1) < p) - 0.5) * 2
+            dV = ( (np.random.normal(0, 1) < p) - 0.5) * 2
             V[t+1] = V[t] + dV
 
         if V[t+1] > V_thresh:
             V[t+1] = V_thresh
     return V
 
+
+# main function
 if __name__ == '__main__':
     T = 10000
     V_reset = -70
@@ -27,24 +29,23 @@ if __name__ == '__main__':
     # find p such that firing rate is approx 10Hz
     firing_rate = 0
     p = 0
-    
+
     while True:
         if abs(firing_rate - 10) < 0.1:
             break
 
         V = generateVoltage(p, T, V_reset, V_thresh, V_0)
-        firing_rate = sum(V==V_thresh) / (T/1000)
+        firing_rate = np.sum(V==V_thresh) / (T/1000)
         p = p + 0.0001
+        print p
 
         if p > 1:
             print 'did not find a suitable p'
             break
-            #break
-    print 'here'
-    exit()
 
     # plot
     time = range(0, T)
-    plt.plot(time,V,
-             xlabel='time (ms)')
-    
+    plt.plot(time,V)
+    plt.xlabel=('time (ms)')
+    plt.ylabel('voltage (mV)')
+    plt.show()
